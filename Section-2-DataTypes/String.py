8adac9568771f8727e22d9ee09bb3c06a6f36f58
DrInnovatio <turing1723@gmail.com>
2020-04-17 21:38:06 +1000

    fishied String part

--- Section-2-DataTypes/String.py
+++ Section-2-DataTypes/String.py
@@ -12,3 +12,19 @@ print(len(myString))
 
 print(myString[len(myString) - 3])
 
+countryCode = "AUS85624526"
+
+countryCode2 = "85624526JAP"
+
+print(countryCode[0:3])
+
+print(countryCode2[-2:-1])
+
+print(countryCode2[-3:])
+
+print(countryCode2[:3])
+
+# print("Hello" + 1234)  An error occurs.
+
+print("Hello-" + str(1234))
+
