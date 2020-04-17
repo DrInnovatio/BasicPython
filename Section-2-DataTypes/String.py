793d1e3f5d2a9dd0cf9750c1a433e233f6bea22d
DrInnovatio <turing1723@gmail.com>
2020-04-17 19:21:28 +1000

    Section 2

---
+++ Section-2-DataTypes/String.py
@@ -0,0 +1,14 @@
+myString = 'She said "meet me at 5."'
+
+print(myString)
+
+print(type(myString))
+
+print(myString[0])
+
+print(myString[-1])
+
+print(len(myString))
+
+print(myString[len(myString) - 3])
+
