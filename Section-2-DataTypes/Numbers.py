18182b70718467c1c7ea410ad92c477e5982127a
DrInnovatio <turing1723@gmail.com>
2020-04-18 21:54:23 +1000

    added math modules

--- Section-2-DataTypes/Numbers.py
+++ Section-2-DataTypes/Numbers.py
@@ -1,3 +1,6 @@
+import math
+# math module
+
 num1 = 5;
 num2 = 3;
 
@@ -6,5 +9,21 @@ print(type(num1))
 result = num1 / num2
 
 print(result)
+
+print( 5 % 3)
+
+print(round(4.87635))
+
+print(round(4.87635, 2))
+
+print(math.factorial(5))
+
+print(math.ceil(2.3))
+
+print(math.floor(2.8))
+
+print(math.log(20))
+
+print(math.pi)
 
 
